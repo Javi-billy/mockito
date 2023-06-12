88aa318b60e5a5398cd15258e3e8967ce2499a52
Javi-billy <74055322+Javi-billy@users.noreply.github.com>
2023-06-12 10:35:10 +0200

    Add files via upload

---
+++ 2_app-mockito/src/main/java/org/appmockito/ejemplos/repositories/ExamenRepository.java
@@ -0,0 +1,10 @@
+package org.appmockito.ejemplos.repositories;
+
+import org.appmockito.ejemplos.models.Examen;
+
+import java.util.List;
+
+public interface ExamenRepository {
+    Examen guardar(Examen examen);
+    List<Examen> findAll();
+}
